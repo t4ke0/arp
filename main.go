ea15c8692d1b948c583ae278e0ad408b3f7606a3
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2021-04-24 23:50:40 +0000

    Refactor the example

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 
@@ -9,46 +10,57 @@ import (
 	"arp/packet"
 )
 
-//NOTE: This where we test the library
+func getHdwAddrByIP(ip, itfc string) (string, error) {
+	c, err := client.New(itfc)
+	if err != nil {
+		return "", err
+	}
+
+	//TODO: steps for getting src ip and src hdwr addr need to be in `client.New` function
+	srcIP, err := client.GetSrcIPAddr(itfc)
+	if err != nil {
+		return "", err
+	}
+
+	srcHdwr, err := client.GetLocalMacAddr(itfc)
+	if err != nil {
+		return "", err
+	}
+
+	dstIP := net.ParseIP(ip).To4()
+
+	pkt, err := packet.MakePacket(packet.REQUEST, srcHdwr, srcIP.To4(), packet.Brodcast, dstIP)
+	if err != nil {
+		return "", err
+	}
+
+	addr, err := c.ResolveAddr(pkt, packet.Brodcast)
+	if err != nil {
+		return "", err
+	}
+
+	return addr.String(), nil
+
+}
 
 func main() {
 
 	itfc := flag.String("interface", "", "network interface")
-
 	internetProtocol := flag.String("ip", "", "destination ip")
-
 	flag.Parse()
-
 	if *itfc == "" || *internetProtocol == "" {
 		flag.PrintDefaults()
 		return
 	}
 
-	c, err := client.New(*itfc)
+	addr, err := getHdwAddrByIP(*internetProtocol, *itfc)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	srcIP, err := client.GetSrcIPAddr(*itfc)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(fmt.Sprintf(`
+TARGET IP: %s
+TARGET MAC: %s
+`, *internetProtocol, addr))
 
-	srcHdwr, err := client.GetLocalMacAddr(*itfc)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dstIP := net.ParseIP(*internetProtocol).To4()
-
-	pkt, err := packet.MakePacket(packet.REQUEST, srcHdwr, srcIP.To4(), packet.Brodcast, dstIP)
-	if err != nil {
-		log.Fatal("ERROR make packet", err)
-	}
-
-	if err := c.SendTO(pkt, packet.Brodcast); err != nil {
-		log.Fatalf("ERROR sending arp packet %v", err)
-	}
-
-	log.Printf("ARP PACKET sent\n")
 }
